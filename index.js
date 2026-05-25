f8d9c42fcfbacb4e080409e0d9749704bfdfe780
Maxim Barbe <112593790+maximbarbe@users.noreply.github.com>
2026-05-25 19:16:14 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const {MongoClient} = require("mongodb");
+
+
+const app = express();
+const PORT = 8383;
+
+
+app.use(express.json());
+dotenv.config()
+
+//https://www.mongodb.com/resources/languages/express-mongodb-rest-api-tutorial
+//https://www.mongodb.com/blog/post/quick-start-nodejs-mongodb-how-to-get-connected-to-your-database
+//https://www.geeksforgeeks.org/node-js/how-to-wait-for-mongodb-connection-to-be-made-before-creating-http-server-in-node-js/
+
+const CONNECTION = process.env.ATLAS_URI || "";
+const DB_CLIENT = new MongoClient(CONNECTION);
+
+
+
+async function main() {
+    try {
+        await DB_CLIENT.connect();
+
+
+        app.get("/", (req, res) => {
+
+            res.send("Hello world").status(200);
+        });
+
+
+        app.listen(PORT, () => {
+            console.log(`Serveur lancé sur http://localhost:${PORT}`);
+        });
+
+
+    } catch(e) {
+        console.error(e);
+    } finally {
+        await DB_CLIENT.close();
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+main().catch(console.error);
