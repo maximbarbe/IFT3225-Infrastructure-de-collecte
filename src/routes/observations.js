5f9da226919c388984ca596853c55e712ce432b3
KamalSkhy <148290222+KamalSkhy@users.noreply.github.com>
2026-05-31 22:27:33 -0400

    Add validation middleware to observations route

--- src/routes/observations.js
+++ src/routes/observations.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const validate = require("../middleware/validate");
+const Observation = require("../models/observation");
 
 
-router.post("/observations", (req, res) => {
+router.post("/observations", validate(observation), (req, res) => {
     res.send("Observations received");
 });
 
