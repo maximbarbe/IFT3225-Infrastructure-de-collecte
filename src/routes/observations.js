7281dbf6ab028ec462e26d92c6be94a40c0a2f5d
maximbarbe <barbe.maxim@gmail.com>
2026-06-02 14:25:49 -0400

    POST request pour observations

--- src/routes/observations.js
+++ src/routes/observations.js
@@ -2,13 +2,23 @@ const express = require('express');
 const {validate} = require("../middleware/validate");
 const {Observation, ObservationPostSchema} = require("../models/observation");
 const {Device} = require("../models/Device");
+const {Location} = require("../models/location")
 const {authenticate} = require("../middleware/auth");
 
 const router = express.Router();
 
-router.post("/observations", [authenticate(Device), validate(ObservationPostSchema)], (req, res) => {
-    // Faire que s'il y a pas de notes, automatique mettre genre Observation.notes = req.body.notes || "No notes"
-    res.send("Observations received");
+router.post("/observations", [authenticate(Device), validate(ObservationPostSchema)], async (req, res) => {
+    const location = await Location.findOne({location: req.body["location"].toLowerCase()});
+    if (!location) {
+        return res.status(400).json({error: "La location n'existe pas, veuillez la créer en utilisant /locations.", details:[]});
+    }
+    const observation = new Observation({...req.body, location: req.body["location"].toLowerCase(), notes: req.body["notes"] || "No notes."});
+    try {
+        await observation.save();
+        res.status(201).json(observation);
+    } catch (e) {
+        res.status(500).json({error: e.message, details: []});
+    }   
 });
 
 
