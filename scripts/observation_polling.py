d16fd96caf8e5b06987a46a88a75be596f55d484
Maxim Barbe <barbe.maxim@gmail.com>
2026-06-05 20:12:41 -0400

    Polling pour les observations, IL FAUT TESTER

---
+++ scripts/observation_polling.py
@@ -0,0 +1,78 @@
+import requests
+import datetime
+import time
+
+PHYPHOX_URL = "http://172.20.10.1/"
+API_URL = "http://localhost:8383"
+
+NO_MINUTES = 20
+
+# À modifier
+LOCATION = "hello"
+
+
+SEUIL_ELEVE = ...
+SEUIL_BAS = ...
+
+start_time = datetime.datetime.now()
+
+
+while (start_time + datetime.timedelta(minutes=NO_MINUTES) > datetime.datetime.now()):
+    # https://phyphox.org/wiki/index.php/Remote-interface_communication
+    # https://phyphox.org/forums/showthread.php?tid=60
+    try:
+        res = requests.get(url=PHYPHOX_URL + "control?cmd=start")
+        if res.status_code != 200:
+            continue
+        print("TRIAL STARTED")
+    except:
+        print("Could not start trial")
+        exit(1)
+    day = datetime.datetime.now()
+    temps_collecte = datetime.datetime.now(datetime.timezone.utc).strftime("%Y-%m-%dT%H:%M:%SZ")
+    time.sleep(5)
+    try:
+        res = requests.get(url=PHYPHOX_URL+"get?dB=full")
+        
+    except:
+        print("could not get data")
+        continue
+    if res.status_code != 200:
+        continue
+    data = res.json()["buffer"]["dB"]["buffer"]
+    print(f"SENDING {len(data)} measurements")
+    for measurement in data:
+        if float(measurement) > SEUIL_ELEVE:
+            vibe = "BRUYANT"
+            proximite = "PROCHE"
+        elif float(measurement) < SEUIL_BAS:
+            vibe = "CALME"
+            proximite = "LOIN"
+        else:
+            vibe = "NORMAL"
+            proximite = "MOYENNE"
+
+        payload = {
+            "location": LOCATION,
+            "vibe": vibe,
+            "proximite": proximite,
+            "notes": f"Timestamp: {temps_collecte}"
+        }
+        try:
+            res = requests.post(API_URL + "/measurements", json=payload, headers={"x-api-key": "test"})
+        except:
+            print("Could not send measurement")
+            print(res.text)
+            exit(1)
+        if res.status_code != 201:
+            print(res.text)
+            print("Could not send data")
+            exit(1)
+    print("BATCH SENT!")
+    try:
+        res = requests.get(url=PHYPHOX_URL + "control?cmd=clear")
+        if res.status_code != 200:
+            continue
+    except:
+        print("Could not stop trial")
+        exit(1)
