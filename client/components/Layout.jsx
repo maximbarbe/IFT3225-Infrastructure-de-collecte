846260fd28961d3ad0591470de45e86452f7f4f1
maximbarbe <barbe.maxim@gmail.com>
2026-07-09 16:41:13 -0400

    Bug fixing (Enlever un display qui faisait que la map apparaissait pas et ajouter des key aux markers)

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -4,7 +4,7 @@ export default function Layout({ children }) {
     return (
         <>
             <Header />
-            <main className="d-flex"> {children} </main>
+            <main> {children} </main>
         </>
     )
 }
--- client/pages/Map.jsx
+++ client/pages/Map.jsx
@@ -2,25 +2,30 @@ import L from "leaflet"
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import Location from "../components/Location";
 
+import "leaflet/dist/leaflet.css";
 
 export default function Map() {
     const locations = [
         {
+            key: 1,
             lat: 40.82400443557466,
             lon: 14.428648136837062,
             desc: "Vesuve"
         },
         {
+            key: 2,
             lat: 45.49623212108189,
             lon: -73.56927238734026,
             desc: "Centre Bell" 
         },
         {
+            key: 3,
             lat: 45.46711932971945, 
             lon: -73.73792317787748,
             desc: "Aéroport International Montréal-Trudeau"
         },
         {
+            key: 4,
             lat: 55.92135547376643,
             lon: 12.679341838376688,
             desc: "Sentier de randonnée suèdois" 
@@ -38,7 +43,7 @@ export default function Map() {
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         {locations.map((loc) => (
-            <Location lat={loc.lat} lon={loc.lon} desc={loc.desc} />
+            <Location key={loc.key} lat={loc.lat} lon={loc.lon} desc={loc.desc} />
         ))}
         
 
