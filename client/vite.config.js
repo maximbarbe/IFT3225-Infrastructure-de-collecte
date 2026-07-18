37d718b1fda52a11c704ee7bb876476b4301a240
maximbarbe <barbe.maxim@gmail.com>
2026-07-18 09:57:21 -0400

    Ajout de .env et .env.example du côté client pour l'url du backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from "dotenv";
+dotenv.config()
 
-const BACKEND_URL = "http://localhost:8383";
+
+const BACKEND_URL = process.env.BACKEND_URL
 
 // https://vite.dev/config/
 export default defineConfig({
