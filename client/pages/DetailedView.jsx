8404f7288b32888530b9140cd3032c598c0b1e5e
Maxim Barbe <barbe.maxim@gmail.com>
2026-08-10 20:36:06 -0400

    Ajout de useMemo pour les graphes de la vue détaillée

--- client/pages/DetailedView.jsx
+++ client/pages/DetailedView.jsx
@@ -6,6 +6,7 @@ import { useAppContext } from '../context/AppContext';
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
 import Table from "../components/Table";
+import { useMemo } from "react";
 
 
 export default function DetailedView() {
@@ -59,82 +60,91 @@ export default function DetailedView() {
     }
     
 
-    let data = []
-    let chart;
-    if (quietHoursData.data) {
-        for (let i = 0; i < 24; i++) {
-            data.push({name: `Hour ${i}`})
-        }
-        const hours = quietHoursData.data.hours
-        for (let i = 0; i < hours.length; i++) {
-            data[hours[i].hour] = {...data[hours[i].hour], "decibels (dB)":hours[i].averageNoise}
-        }
-    // Le code pour le bar chart a été tiré de (Recharts, s.d.a) et adapté à nos fins.
-    chart = <BarChart
-      style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
-      responsive
-      data={data}
-      margin={{
-        top: 5,
-        right: 0,
-        left: 0,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" />
-      <YAxis width="auto" />
-      <Tooltip />
-      <Legend />
-      <Bar dataKey="decibels (dB)" fill="#8884d8" activeBar={{ fill: 'pink', stroke: 'blue' }} radius={[10, 10, 0, 0]} />
-    </BarChart>;
-    }
-    const historicalGraphData = []
-
-    if (historyData.data) {
-        for (let dataPoint of historyData.data.series) {
-            historicalGraphData.push({"name": dataPoint.bucketStart, "decibels (dB)": dataPoint.averageNoise})
-        }
-    }
-
-    // Le code pour le line chart a été tiré de (Recharts, s.d.b) et adapté à nos fins
-    // Le fix pour le bug que la ligne n'apparaissait pas provient du code écrit dans ce post.
-    // Il s'agit du stroke="#000000" qui a fix le problème. (LoF10, 2019)
-    const lineChart =         <LineChart
-      style={{ width: '100%', maxWidth: '700px', height: '100%', maxHeight: '70vh', aspectRatio: 1.618 }}
-      responsive
-      data={historicalGraphData}
-      margin={{
-        top: 5,
-        right: 0,
-        left: 0,
-        bottom: 5,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="name" stroke="var(--color-text-3)" tick={false}/>
-      <YAxis width="auto" stroke="var(--color-text-3)" />
-      <Tooltip
-        cursor={{
-          stroke: 'var(--color-border-2)',
+    
+    
+    const barChart = useMemo(() => {
+        let chart;
+        let data = []
+        if (quietHoursData.data) {
+            for (let i = 0; i < 24; i++) {
+                data.push({name: `Hour ${i}`})
+            }
+            const hours = quietHoursData.data.hours
+            
+            for (let i = 0; i < hours.length; i++) {
+                data[hours[i].hour] = {...data[hours[i].hour], "decibels (dB)":hours[i].averageNoise}
+            }
+        // Le code pour le bar chart a été tiré de (Recharts, s.d.a) et adapté à nos fins.
+        chart = <BarChart
+        style={{ width: '100%', maxWidth: '700px', maxHeight: '70vh', aspectRatio: 1.618 }}
+        responsive
+        data={data}
+        margin={{
+            top: 5,
+            right: 0,
+            left: 0,
+            bottom: 5,
         }}
-        contentStyle={{
-          backgroundColor: 'var(--color-surface-raised)',
-          borderColor: 'var(--color-border-2)',
-        }}
-      />
-      <Legend />
-      <Line
-        type="monotone"
-        dataKey="decibels (dB)"
-        stroke="#000000"
-        dot={{
-          fill: '#000000',
-        }}
-        activeDot={{ r: 8, stroke: 'var(--color-surface-base)' }}
-      />
-    </LineChart>
+        >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" />
+        <YAxis width="auto" />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="decibels (dB)" fill="#8884d8" activeBar={{ fill: 'pink', stroke: 'blue' }} radius={[10, 10, 0, 0]} />
+        </BarChart>;
+        }
+        return chart
+    }, [quietHoursData]) 
+    
+    const lineChart = useMemo(() => {
+        const historicalGraphData = []
 
+        if (historyData.data) {
+            for (let dataPoint of historyData.data.series) {
+                historicalGraphData.push({"name": dataPoint.bucketStart, "decibels (dB)": dataPoint.averageNoise})
+            }
+        }
+
+        // Le code pour le line chart a été tiré de (Recharts, s.d.b) et adapté à nos fins
+        // Le fix pour le bug que la ligne n'apparaissait pas provient du code écrit dans ce post.
+        // Il s'agit du stroke="#000000" qui a fix le problème. (LoF10, 2019)
+        const lc =         <LineChart
+        style={{ width: '100%', maxWidth: '700px', height: '100%', maxHeight: '70vh', aspectRatio: 1.618 }}
+        responsive
+        data={historicalGraphData}
+        margin={{
+            top: 5,
+            right: 0,
+            left: 0,
+            bottom: 5,
+        }}
+        >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" stroke="var(--color-text-3)" tick={false}/>
+        <YAxis width="auto" stroke="var(--color-text-3)" />
+        <Tooltip
+            cursor={{
+            stroke: 'var(--color-border-2)',
+            }}
+            contentStyle={{
+            backgroundColor: 'var(--color-surface-raised)',
+            borderColor: 'var(--color-border-2)',
+            }}
+        />
+        <Legend />
+        <Line
+            type="monotone"
+            dataKey="decibels (dB)"
+            stroke="#000000"
+            dot={{
+            fill: '#000000',
+            }}
+            activeDot={{ r: 8, stroke: 'var(--color-surface-base)' }}
+        />
+        </LineChart>
+        return lc
+    }, [historyData.data])
 
     let cName;
 
@@ -176,7 +186,7 @@ export default function DetailedView() {
                     <h3>Classification d'ambiance courante: <span className={cName}>{ambianceData.data.noiseLevel.toUpperCase()}</span></h3>
                 </div>
                 <h4>Graphique montrant les niveaux sonores moyens pour les heures de la journée (UTC).</h4>
-                {chart}
+                {barChart}
                 <h4>Graphique montrant l'historique par bloc de 15 minutes (Une vue tabulaire est plus bas) (UTC).</h4>
                 {lineChart}
 
