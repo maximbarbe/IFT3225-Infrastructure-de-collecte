2bd81b87b63e44542349c247a67188290c9344cf
KamalSkhy <148290222+KamalSkhy@users.noreply.github.com>
2026-08-11 23:06:03 -0400

    ajout getrecentobserv

--- client/services/observation.js
+++ client/services/observation.js
@@ -1,9 +1,40 @@
 import callApi from "./apiCaller";
 
+
+// Soumettre une observation
 export async function postObservation(observation, token) {
-    return callApi("/observations", "POST", { "Content-Type": "application/json", "Authorization": `Bearer ${token}`}, JSON.stringify({...observation}));
+    return callApi(
+        "/observations",
+        "POST",
+        {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+        JSON.stringify({ ...observation })
+    );
 }
 
+
+// Récupérer les observations de l'utilisateur connecté
 export async function getMyObservations(token) {
-    return callApi("/observations", "GET", { "Content-Type": "application/json", "Authorization": `Bearer ${token}`});
+    return callApi(
+        "/observations",
+        "GET",
+        {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+    );
+}
+
+
+// Récupérer les 5 observations les plus récentes d'un lieu
+export async function getRecentObservations(location) {
+    return callApi(
+        `/observations/location/${encodeURIComponent(location)}?limit=5`,
+        "GET",
+        {
+            "Content-Type": "application/json"
+        }
+    );
 }
