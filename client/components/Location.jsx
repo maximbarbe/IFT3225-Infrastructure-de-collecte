647214ce0ad1424cabe077ca3597f4b044a82205
Maxim Barbe <112593790+maximbarbe@users.noreply.github.com>
2026-08-13 19:05:31 -0400

    Refactoring pour rendre les marqueurs customs encore plus réutilisable

--- client/components/Location.jsx
+++ client/components/Location.jsx
@@ -12,9 +12,9 @@ export default function Location({ lat, lon, loc }) {
 
     const {data, loading, error} = useApi(() => (getAmbiance(loc, "2160h")))
     return(<>
-        {(!loading &&!error) && <CustomMarker lat={lat} lon={lon} loc={loc} msg={`Classification ambiante: ${data.noiseLevel.toUpperCase()}`}/>}
-        {!error &&loading && <CustomMarker lat={lat} lon={lon} loc={loc} textType={"text-warning"} msg={`La classification est entrain de charger. Si ca prend trop longtemps, veuillez rafraîchir la page.`}/>}
-        {error && <CustomMarker lat={lat} lon={lon} loc={loc} textType={"text-danger"} msg={`Il y a eu un erreur lors du chargement de cette location. Veuillez rafraichir la page ou appuyer sur le marqueur pour en voir plus.`}/>}
+        {(!loading &&!error) && <CustomMarker lat={lat} lon={lon} loc={loc} clickFunction={() => navigate(`/view/${loc}`)} msg={`Classification ambiante: ${data.noiseLevel.toUpperCase()}`}/>}
+        {!error &&loading && <CustomMarker lat={lat} lon={lon} loc={loc} clickFunction={() => navigate(`/view/${loc}`)} textType={"text-warning"} msg={`La classification est entrain de charger. Si ca prend trop longtemps, veuillez rafraîchir la page.`}/>}
+        {error && <CustomMarker lat={lat} lon={lon} loc={loc} clickFunction={() => navigate(`/view/${loc}`)} textType={"text-danger"} msg={`Il y a eu un erreur lors du chargement de cette location. Veuillez rafraichir la page ou appuyer sur le marqueur pour en voir plus.`}/>}
         </>  
     )   
 }
