6a96905bcdeb4559998666265c40cc57958e45aa
Maxim Barbe <112593790+maximbarbe@users.noreply.github.com>
2026-08-12 20:29:10 -0400

    Refactoring observation.js

--- client/services/observation.js
+++ client/services/observation.js
@@ -1,40 +1,13 @@
 import callApi from "./apiCaller";
 
-
-// Soumettre une observation
 export async function postObservation(observation, token) {
-    return callApi(
-        "/observations",
-        "POST",
-        {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-        },
-        JSON.stringify({ ...observation })
-    );
+    return callApi("/observations", "POST", { "Content-Type": "application/json", "Authorization": `Bearer ${token}`}, JSON.stringify({...observation}));
 }
 
-
-// Récupérer les observations de l'utilisateur connecté
 export async function getMyObservations(token) {
-    return callApi(
-        "/observations",
-        "GET",
-        {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token}`
-        }
-    );
+    return callApi("/observations", "GET", { "Content-Type": "application/json", "Authorization": `Bearer ${token}`});
 }
 
-
-// Récupérer les 5 observations les plus récentes d'un lieu
 export async function getRecentObservations(location) {
-    return callApi(
-        `/observations/location/${encodeURIComponent(location)}?limit=5`,
-        "GET",
-        {
-            "Content-Type": "application/json"
-        }
-    );
+    return callApi(`/observations/${location}?limit=5`, "GET", {"Content-Type": "application/json"})
 }
